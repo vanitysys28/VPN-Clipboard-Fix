b7f0365e20d4234347c96ab82a3262e183dcbfbf
SerenityV8 <aubert.guillaume26@gmail.com>
2021-10-08 09:26:06 +0200

    Update background.js

--- background.js
+++ background.js
@@ -13,3 +13,34 @@ chrome.commands.onCommand.addListener(function(command) {
         alert("Texte prêt à être collé")
     }
 });
+
+
+var keys;
+ 
+    document.addEventListener("keydown", function (e) {
+        keys = (Object.keys || []);
+        keys[e.keyCode]=true;
+
+         if (keys[17] && keys[18] && keys[67]){
+            document.getElementById('frds_clipboard_text').value = Module.ccall('frds_sdl_rdp_send_clipboard_req', null,
+			 ['number'], [frds_ctx_ptr]);
+            frds.open_menu(); 
+// 	      document.getElementById("frds_clipboard_ctrl").style.display = "block"
+//            Module.ccall('frds_sdl_set_keyboard_event', ['boolean'],
+// 		  ['0']);
+     }
+      if (keys[17] && keys[18] && keys[83]){
+            frds.open_menu();
+     }
+     if (keys[17] && keys[18] && keys[86]){
+            frds.on_clipboard_to_remote()
+            frds.close_clipboard()
+     }
+    } , false);
+
+
+
+document.addEventListener("keyup", function (e) {
+keys[e.keyCode]=false;
+stop();
+ } , false);
