2f547df933c2fedcd2b675eed99c6886631f2ea1
SerenityV8 <aubert.guillaume26@gmail.com>
2021-10-08 10:38:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var s = document.createElement('script');
-s.src = chrome.runtime.getURL('commands.js');
+s.src = chrome.runtime.getURL('shortcuts.js');
 s.onload = function() {
     this.remove();
 };
